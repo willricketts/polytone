03dd72ede5e07424c9e051d30c54a1d018e4dd9b
willricketts <will@willricketts.com>
2015-04-21 16:56:09 -0400

    added first bit of contact form

--- public/javascripts/behaviors.js
+++ public/javascripts/behaviors.js
@@ -30,4 +30,30 @@ $(document).ready(function() {
     }, 800);
     return false;
   });
+
+  $('#contactSubmit').click(function() {
+
+    var name = $("#nameField").val();
+    var phone = $("#phoneField").val();
+    var message = $("#messageField").val();
+
+    var data = {
+      name: name,
+      phone: phone,
+      message: message
+    };
+
+    $.ajax({
+      type: "POST",
+      url: '/contact',
+      data: data
+    }, function(result) {
+      if(data == { status: 'success' }) {
+        return false;
+      }
+      else {
+        alert('There was a problem');
+      }
+    });
+  });
 });
