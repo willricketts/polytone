3f05c30bdccc6cb936c363e3c17568cdd7a8792c
willricketts <will@willricketts.com>
2015-04-21 11:19:22 -0400

    fixed white flash in click scroll jquery animation

--- public/javascripts/behaviors.js
+++ public/javascripts/behaviors.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
+
+  $('#hello').hide();
+  $("#hello").fadeIn(1000);
+
   $('#developmentNav').click(function() {
     $('html, body').animate({
       scrollTop: $("#development").offset().top
     }, 800);
+    return false;
   });
 
   $('#designNav').click(function() {
     $('html, body').animate({
       scrollTop: $("#design").offset().top
     }, 800);
+    return false;
   });
 
   $('#consultingNav').click(function() {
     $('html, body').animate({
       scrollTop: $("#consulting").offset().top
     }, 800);
+    return false;
   });
 
   $('#startNav').click(function() {
     $('html, body').animate({
       scrollTop: $("#start").offset().top
     }, 800);
+    return false;
   });
 });
